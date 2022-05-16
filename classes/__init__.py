2b048b9e3ecf21bfc8f3ce8a3e3238c9325abb4b
Will Musgrave <wl317861@dal.ca>
2022-05-16 18:11:11 -0300

    Ensure all classes can be found in module

---
+++ classes/__init__.py
@@ -0,0 +1,6 @@
+# flake8: noqa
+from classes.custom_errors import LineTokenizationError, StandardDefinitionParseError
+from classes.processor import Processor
+from classes.token_processor import TokenProcessor
+from classes.line_processor import LineProcessor
+from classes.generator import Generator
